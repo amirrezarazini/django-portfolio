218d963da865d6dcbe172d83d537b645af825a3d
amirrezarazini <amirrezarazini@gmail.com>
2025-06-15 21:19:38 +0330

    Fix ALLOWED_HOSTS for production

--- A/settings.py
+++ A/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-hlfo&&l^qe5c1=seb7!$dphy%g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['portfolio-app-r9vd.onrender.com']
 
 # Application definition
 
