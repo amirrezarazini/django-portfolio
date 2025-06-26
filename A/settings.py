877711c1920d030b278a3dd19a88d50c9c6010c2
amirrezarazini <amirrezarazini@gmail.com>
2025-06-27 02:27:54 +0330

    Temporary: enable DEBUG for media files

--- A/settings.py
+++ A/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-hlfo&&l^qe5c1=seb7!$dphy%g9xbzh*7m=(9khfozm$kv29b=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['portfolio-app-r9vd.onrender.com']
 
 # Application definition
 
