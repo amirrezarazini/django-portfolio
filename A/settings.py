8ec7690f0bb971b72b8c13e65a4f9fce62e0bf8f
amirrezarazini <amirrezarazini@gmail.com>
2025-06-30 17:27:38 +0330

    توضیح کوتاه درباره تغییرات

--- A/settings.py
+++ A/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['raziniar.ir', 'www.raziniar.ir', '127.0.0.1', 'localhost']
 
